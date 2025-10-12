ccbe99b4130ebfab7817ee9b6e29b95c991fbc18
hugo <hugosanzfrias74@gmail.com>
2025-10-12 21:35:44 +0200

    ExercicisProgramació

--- Exercici 6/Exercisi6.py
+++ Exercici 6/Exercisi6.py
@@ -1,8 +1,9 @@
-# Supón que x y y son variables de tipo float
+# Numero X y Y de tipo float ja que tenn decimals.
 x = float(input("Número X: "))
 y = float(input("Número Y: "))
 
-# Calcula z según la fórmula dada
+# Calcul de la fórmula.
 z = (1 + (x**2 / y)) / ((x**3) / (1 + y))
 
+#Resultat de la fórmula.
 print(f"Este es el resultat: {z}")
