e5c271b48555f9b9cdc49c0d64af3ef58fff0261
Hugo <152479863+HugoSanz@users.noreply.github.com>
2026-02-23 17:10:41 +0100

    Delete Exercici 6 directory

--- Exercici 6/Exercisi6.py
+++
@@ -1,9 +0,0 @@
-# Numero X y Y de tipo float ja que tenn decimals.
-x = float(input("Número X: "))
-y = float(input("Número Y: "))
-
-# Calcul de la fórmula.
-z = (1 + (x**2 / y)) / ((x**3) / (1 + y))
-
-#Resultat de la fórmula.
-print(f"Este es el resultat: {z}")
